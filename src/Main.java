c0bc1a05f254050ee3b1dd09ed90d424964b4ac3
k.jogith jaya siva manikanta <jogithgdv9@gmail.com>
2026-02-27 15:45:57 +0530

    UC9:Recursive Palindrome Checker

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,28 @@
 import java.util.Scanner;
-import java.util.LinkedList;
 
-class PalindromeCheckerApp{
+class PalindromeCheckerApp {
+
+    public static boolean isPalindrome(String str) {
+
+        if (str.length() <= 1) {
+            return true;
+        }
+
+        if (str.charAt(0) != str.charAt(str.length() - 1)) {
+            return false;
+        }
+
+        return isPalindrome(str.substring(1, str.length() - 1));
+    }
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("system initialized successfully");
 
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-
-        LinkedList<Character> list = new LinkedList<>();
-
-        for (char c : input.toCharArray()) {
-            list.add(c);
-        }
-
-        boolean isPalindrome = true;
-
-        while (list.size() > 1) {
-            char first = list.removeFirst();
-            char last = list.removeLast();
-
-            if (first != last) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if (isPalindrome) {
+        if (isPalindrome(input)) {
             System.out.println("Is it a Palindrome? : true");
         } else {
             System.out.println("Is it a Palindrome? : false");
