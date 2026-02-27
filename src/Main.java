9e551c54b02401c1c5f08326153327530ae226c4
k.jogith jaya siva manikanta <jogithgdv9@gmail.com>
2026-02-27 14:28:01 +0530

    UC1: Add welcome message module

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized successfully");
+
+    }
+}
